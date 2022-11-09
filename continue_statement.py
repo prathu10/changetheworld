6901d1836c9fa4629c0153955dafb7504e62ac5b
prathu10 <108126800+prathu10@users.noreply.github.com>
2022-11-09 19:23:26 +0530

    Add files via upload

---
+++ continue_statement.py
@@ -0,0 +1,7 @@
+#continue is used to stop the current iteration of the loop and continue with the next one.
+# It instructs the python to"skip this iteration" 
+
+if a in range(10):
+    if a == 5:
+     continue
+print(a)
